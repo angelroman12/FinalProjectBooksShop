31978c471ee5f8ad2ea63a53fef4d8a514ec6071
“Andrei <romanangel95@yahoo.com>
2021-10-01 21:19:52 +0300

    btn filter& mobile style

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import { http } from './http.js';
 import { ui } from './ui.js';
+import { uiFilter} from './uiFilter.js'
 // Get Products on DOM load
 document.addEventListener('DOMContentLoaded', getProducts);
 
@@ -10,39 +11,46 @@ function getProducts() {
 }
 
 
+let inStoc = document.getElementById('inStoc').addEventListener('click', () => {
+	output.innerHTML = ''
+	http
+	.get("https://6086e5aea3b9c200173b7295.mockapi.io/products")
+	.then((data) => uiFilter.stock(data))
+})
+
 let btn = document.querySelector('#fil')
 
 btn.addEventListener('click', ()=> {
 	output.innerHTML = ''
 	http
 	.get("https://6086e5aea3b9c200173b7295.mockapi.io/products")
-	.then((data) => ui.cheap(data))
+	.then((data) => uiFilter.cheap(data))
 })
 
 let filter2130 = document.getElementById('21-30').addEventListener('click', () => {
 	output.innerHTML = ''
 	http
 	.get("https://6086e5aea3b9c200173b7295.mockapi.io/products")
-	.then((data) => ui.cheap2130(data))
+	.then((data) => uiFilter.cheap2130(data))
 })
 
 let filter3140 = document.getElementById('31-40').addEventListener('click', () => {
 	output.innerHTML = ''
 	http
 	.get("https://6086e5aea3b9c200173b7295.mockapi.io/products")
-	.then((data) => ui.cheap3140(data))
+	.then((data) => uiFilter.cheap3140(data))
 })
 
 let filter4150 = document.getElementById('41-50').addEventListener('click', () => {
 	output.innerHTML = ''
 	http
 	.get("https://6086e5aea3b9c200173b7295.mockapi.io/products")
-	.then((data) => ui.cheap4150(data))
+	.then((data) => uiFilter.cheap4150(data))
 })
 
 let filter51 = document.getElementById('51').addEventListener('click', () => {
 	output.innerHTML = ''
 	http
 	.get("https://6086e5aea3b9c200173b7295.mockapi.io/products")
-	.then((data) => ui.cheap51(data))
+	.then((data) => uiFilter.cheap51(data))
 })
---
+++ src/uiFilter.js
@@ -0,0 +1,125 @@
+
+class UIfilter {
+    constructor() {		
+		this.productsDiv = document.getElementById('output');		
+		this.picture = document.getElementById('picture');
+		this.author = document.getElementById('author');
+		this.title = document.getElementById('title');
+		this.id = document.getElementById('id');
+		this.price = document.getElementById('price');		
+		this.description = document.getElementById('description');
+		this.tableBody = document.getElementById('table-body');
+        this.cartBody = document.getElementById('cart-body');
+		this.quantity = document.getElementById('quantity');
+        this.total = document.getElementById('total');
+        this.subtotal = document.getElementById('subtotal');
+		this.modal = document.querySelector('.modal');
+		this.detCont = document.querySelector('#output')
+		this.searchInput = document.getElementById('search')
+	}
+
+	cheap(products) {
+	 	let filtrate = products.filter(product => product.price < 20)
+		let output = ''
+		filtrate.forEach((product) => {
+            output =
+			`
+            <div class="card">
+            <a href="details.html?id=${product.id}" ><img src="${product.picture}"> </a>
+            <h4> ${product.author}</h4>
+            <h5>${product.title}</h5>
+            <p>${product.price} ${product.curency} </p> 
+        	<a href="details.html?id=${product.id}"> <button class="det add-cart"> Detalii</button> </a>					
+            </div>            
+            `;
+			this.productsDiv.innerHTML += output;
+		});
+	}
+	cheap2130(products) {
+		let filtrate = products.filter(product => product.price >= 21 && product.price <= 30)
+	    let output = ''
+	    filtrate.forEach((product) => {
+		   output =
+		   `
+		   <div class="card">
+		   <a href="details.html?id=${product.id}" ><img src="${product.picture}"> </a>
+		   <h4> ${product.author}</h4>
+		   <h5>${product.title}</h5>
+		   <p>${product.price} ${product.curency} </p> 
+		   <a href="details.html?id=${product.id}"> <button class="det add-cart"> Detalii</button> </a>					
+		   </div>            
+		   `;
+		   this.productsDiv.innerHTML += output;
+	   });
+   }
+   cheap3140(products) {
+	let filtrate = products.filter(product => product.price >= 31 && product.price <= 40)
+    let output = ''
+    filtrate.forEach((product) => {
+	   output =
+	   `
+	   <div class="card">
+	   <a href="details.html?id=${product.id}" ><img src="${product.picture}"> </a>
+	   <h4> ${product.author}</h4>
+	   <h5>${product.title}</h5>
+	   <p>${product.price} ${product.curency} </p> 
+	   <a href="details.html?id=${product.id}"> <button class="det add-cart"> Detalii</button> </a>					
+	   </div>            
+	   `;
+	   this.productsDiv.innerHTML += output;
+   });
+}
+	cheap4150(products) {
+	let filtrate = products.filter(product => product.price >= 41 && product.price <= 50)
+    let output = ''
+    filtrate.forEach((product) => {
+	   output =
+	   `
+	   <div class="card">
+	   <a href="details.html?id=${product.id}" ><img src="${product.picture}"> </a>
+	   <h4> ${product.author}</h4>
+	   <h5>${product.title}</h5>
+	   <p>${product.price} ${product.curency} </p> 
+	   <a href="details.html?id=${product.id}"> <button class="det add-cart"> Detalii</button> </a>					
+	   </div>            
+	   `;
+	   this.productsDiv.innerHTML += output;
+   });
+}
+	cheap51(products) {
+		let filtrate = products.filter(product => product.price >= 51)
+    	let output = ''
+    	filtrate.forEach((product) => {
+	 	  output =
+	   		`
+	   		<div class="card">
+	   		<a href="details.html?id=${product.id}" ><img src="${product.picture}"> </a>
+	   		<h4> ${product.author}</h4>
+	   		<h5>${product.title}</h5>
+	   		<p>${product.price} ${product.curency} </p> 
+	   		<a href="details.html?id=${product.id}"> <button class="det add-cart"> Detalii</button> </a>					
+	   		</div>            
+	  		`;
+	   this.productsDiv.innerHTML += output;
+   });
+}
+	stock(products) {
+		let filtrate = products.filter(product => product.stoc)
+		let output = ''
+    	filtrate.forEach((product) => {
+	 	  output =
+	   		`
+	   		<div class="card">
+	   		<a href="details.html?id=${product.id}" ><img src="${product.picture}"> </a>
+	   		<h4> ${product.author}</h4>
+	   		<h5>${product.title}</h5>
+	   		<p>${product.price} ${product.curency} </p> 
+	   		<a href="details.html?id=${product.id}"> <button class="det add-cart"> Detalii</button> </a>					
+	   		</div>            
+	  		`;
+	   this.productsDiv.innerHTML += output;
+   });
+	}
+}
+
+export const uiFilter = new UIfilter();
